ac3730c6a39c3b0846e61425f24a6b8730016c45
Diana <isakovdiana1@gmail.com>
2021-10-06 14:02:35 +0300

    .env config

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import mybagRoutes from './routes/mybag.js'
 
 const app = express();
-
+dotenv.config();
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,10 +15,9 @@ app.use(cors());
 
 app.use('/mybag', mybagRoutes);
 
-const CONECTION_URL = "mongodb+srv://scrapmybag:scrapmybag123123@cluster0.xinpo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server runing on port: ${PORT}`)))
     .catch((err) => console.log(err.message));
 
